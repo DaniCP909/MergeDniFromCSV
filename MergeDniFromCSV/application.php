8b9d0ccff9e13b5148a58daf34a9b813f09de89b
DaniCP909 <d.corredorpadrino@gmail.com>
2023-12-04 13:06:41 +0100

    clean application.php

--- MergeDniFromCSV/application.php
+++ MergeDniFromCSV/application.php
@@ -11,19 +11,4 @@ $application = new Application();
 
 $application->add(new Helloworldcommand());
 $application->add(new MergerCommand());
-/*
-$test_csv_file = new CsvReader('fichero.csv');
-$test_csv_file->printRCsv();
-
-echo "\n";
-
-$wcsv = new CsvWriter('solucion.csv');
-$wcsv->setNewRecord(array('Dani','Corredor', 'Padrino'));
-$wcsv->setNewRecord(array('Javi', 'Nogues', 'Alguacil'));
-$wcsv->setNewRecord(array('Fernando', 'Alonso', 'Díaz'));
-$wcsv->setNewRecord(array('campo4'));
-$wcsv->writeCsv();
-
-$wcsv->printWCsv() . "\n";
-*/
 $application->run();
