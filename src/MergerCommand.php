6c115d3b60ca28f51d3e2c63b6c7b359a1ded28a
DaniCP909 <d.corredorpadrino@gmail.com>
2023-12-11 08:24:42 +0100

    clean MergeCommand.php + Delete CSV

--- src/MergerCommand.php
+++ src/MergerCommand.php
@@ -70,9 +70,6 @@ class MergerCommand extends Command
             
             $destinationFile->insertOne($record);
         }
-        /*
-        
-        */
 
         return Command::SUCCESS;
     }
---
+++ tests/MergeDniTest.php
@@ -0,0 +1,7 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+final class MergeDniTest extends TestCase 
+{
+    
+}
